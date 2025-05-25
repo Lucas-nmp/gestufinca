f47e2ae8f2be442d85fd6837fbd908effb64b74e
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-05-25 17:15:05 +0200

    Added controller and crud news

---
+++ src/Form/NewsForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('news_date')
+            ->add('body_news')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
