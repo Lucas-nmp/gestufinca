4e1c8d5afe4a0f8f7cd3d4c34a20a5d21d33dd87
lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-06-01 00:39:24 +0200

    Added desing to form archives

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,23 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 use App\Repository\NewsRepository;
+use App\Repository\OpinionsRepository;
 
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(NewsRepository $newsRepository): Response
+    public function index(NewsRepository $newsRepository, OpinionsRepository $opinionRepository): Response
     {
         // Fetching the latest news items to display on the home page
         $latestNews = $newsRepository->findBy([], ['id' => 'DESC'], 6);
-        // Render the home page with the latest news
+    
         
         // Do the same with opinions
+        $latestOpinions = $opinionRepository->findBy([], ['id' => 'DESC'], 6);
+
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'latestNews' => $latestNews,
-            'newsCount' => $newsRepository->count([]),
+            'latestOpinions' => $latestOpinions,
         ]);
     }
 }
